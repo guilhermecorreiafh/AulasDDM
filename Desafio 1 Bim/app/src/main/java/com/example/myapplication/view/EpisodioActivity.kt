87975080c2273e1a9c6585a874cd6b4f4ac4d0f8
joaokbrau <joaovictorcs211204@gmail.com>
2026-09-11 21:16:47 -0300

    fix: Ajustes lista

---
+++ Desafio 1 Bim/app/src/main/java/com/example/myapplication/view/EpisodioActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.adapter.UsuarioAdapter
+import com.example.myapplication.dao.UsuarioDao
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class EpisodioActivity : AppCompatActivity(R.layout.lista_episodios){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+}
